d8d6ad9225b4031252b79ccb2c6228476a317fb7
Z-Akdim <zakaria.akdim37@gmail.com>
2025-05-13 15:26:13 +0100

    feat: create tabs bar with coustom style

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,19 @@
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, ScrollView, View } from "react-native";
 
 const Home = () => {
   return (
-    <View>
-      <Text>home</Text>
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute w-full z-0" />
+      <ScrollView
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
+      >
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+      </ScrollView>
     </View>
   );
 };
